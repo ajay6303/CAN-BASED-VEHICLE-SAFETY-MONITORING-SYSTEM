a256e0a2fef144d10330e03dd8d1bf89d37974c2
Ajay Kumar Pilla <143744923+ajay6303@users.noreply.github.com>
2026-05-26 16:58:42 +0530

    Update can_defines.h

--- can_defines.h
+++ can_defines.h
@@ -1,74 +1,44 @@
 #ifndef _CAN_DEFINES_H_
 #define _CAN_DEFINES_H_
 
-//TD!(CAN1_TX)pin no alternate func,default
+// --- Physical Layout Pin Settings Configurations ---
+#define RD1_PIN 0x00040000           // Bit configuration setting pin P0.25 function paths to CAN1 RX (RD1)
 
-//#define TD1_PIN
+// --- Advanced Network Clock Frequency Calculation Constants ---
+#define PCLK 15000000                // Base Peripheral Clock frequency set to 15 MHz
+#define BIT_RATE 125000              // Target operational network data transmission speed set to 125 kbps
+#define QUANTA 20                    // Total time quanta count slices assigned per single bit width window
+#define BRP (PCLK/(BIT_RATE*QUANTA)) // Baud Rate Prescaler logic calculation equation value
+#define SAMPLE_POINT (0.7*QUANTA)    // Fix optimal sample checkpoint metrics inside 70% bit width envelopes
+#define TSEG1 ((u32)SAMPLE_POINT-1)  // Time Segment 1 tick count definition block mapping
+#define TSEG2 (QUANTA-(1+TSEG1))     // Time Segment 2 tick count definition block mapping
 
-//define p0.25 as RD1(i.e CAN1_RX)
-
-#define RD1_PIN 0x00040000
-
-
-#define PCLK 15000000 //hz
-
-#define BIT_RATE 125000 //hz
-
-#define QUANTA 20
-
-#define BRP (PCLK/(BIT_RATE*QUANTA))
-
-#define SAMPLE_POINT (0.7*QUANTA)
-
-#define TSEG1 ((u32)SAMPLE_POINT-1)
-
-#define TSEG2 (QUANTA-(1+TSEG1))
-//synchronization jump width
+// Synchronization Jump Width constraints setup rules definitions mapping
 #define SJW ((TSEG2>=5)?4:(TSEG2-1))
 
-
-//cfg values for CxBTR sfr
-//bit timing register
-#define SAM 0 //0/1,sample bus 1/3 time's
-
+// --- Aggregated Constant Bit Map Config Blocks for BTR Control Registers ---
+#define SAM 0 // Bus sampling configuration choice (0 = Sample single time, 1 = Sample three times)
 #define BTR_LVAL (SAM<<23|((TSEG2-1)<<20)|((TSEG1-1)<<16)|((SJW-1)<<14)|(BRP-1))
 
+// --- Command Register Bit Positions (CxCMR SFR) ---
+#define TR_BIT 0   // Transmission Request command bit configuration tracker
+#define RRB_BIT 2  // Release Receive Buffer command bit configuration tracker
+#define STB1_BIT 5 // Select Transmit Buffer 1 command bit configuration tracker
 
-//CxCMR sfr bit defines
+// --- Global Status Register Bit Positions (CxGSR SFR) ---
+#define RBS_BIT 0   // Receive Buffer Status alert indicator position track
+#define TBS1_BIT 2  // Transmit Buffer Status 1 vacancy tracking indicator position
+#define TCS1_BIT 3  // Transmission Compliance Status validation tracking indicator position
 
-#define TR_BIT 0
+// --- Transmission Frame Info & Receive Frame Status Parameter Bit Envelopes ---
+#define RTR_BIT 30   // Offset bit alignment tracking for Remote Transmission Requests data
+#define DLC_BITS 16  // Offset bit alignment tracking for Data Length Code byte parameters
 
-#define RRB_BIT 2
+// --- Mode Control Register Configuration Targets (CxMOD SFR) ---
+#define RM_BIT 0     // Reset Mode operational status override switch indicator positioning
 
-#define STB1_BIT 5
-
-
-//CxGSR sfr bit defines 
-
-#define RBS_BIT 0
-
-#define TBS1_BIT 2
-
-#define TCS1_BIT 3
-
-
-
-//CxTFI1/CxRFS sfr bit defines
-
-#define RTR_BIT 30
-
-#define DLC_BITS 16
-
-
-//CxMOD sfr bit defines
-
-#define RM_BIT 0
-
-
-//AFMR sfr bit defines
-
-#define AccOFF_BIT 0
-
-#define AccBP_BIT  1
+// --- Acceptance Filter Mode Register Option Controls (AFMR SFR) ---
+#define AccOFF_BIT 0 // Main Acceptance Filter Off control lane selector position switch
+#define AccBP_BIT  1 // Acceptance Filter Bypass control lane selector position switch
 
 #endif
