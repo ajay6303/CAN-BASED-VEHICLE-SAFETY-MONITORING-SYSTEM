9feb884cf432c7bc4911f39a85d61e86298ac716
Ajay Kumar Pilla <143744923+ajay6303@users.noreply.github.com>
2026-05-26 16:59:41 +0530

    Update delay.h

--- delay.h
+++ delay.h
@@ -1,12 +1,11 @@
 #ifndef _DELAY_H_
 #define _DELAY_H_
+
 typedef unsigned int u32;
 
-void delay_us(u32 dlyus);
-
-void delay_ms(u32 dlyms);
-
-void delay_s(u32 dlys);
+// --- Time Pacing Inclusions API ---
+void delay_us(u32 dlyus); // Software busy-wait tracking microsecond iterations
+void delay_ms(u32 dlyms); // Software busy-wait tracking millisecond iterations
+void delay_s(u32 dlys);   // Software busy-wait tracking whole second iterations
 
 #endif
-
