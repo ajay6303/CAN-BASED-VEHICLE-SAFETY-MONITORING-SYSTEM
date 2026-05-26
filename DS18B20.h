82ac6a1daae213eebae3a46286581d536738caef
Ajay Kumar Pilla <143744923+ajay6303@users.noreply.github.com>
2026-05-26 16:55:38 +0530

    Update DS18B20.h

--- DS18B20.h
+++ DS18B20.h
@@ -1,11 +1,12 @@
 #ifndef _DS18B20_H_
 #define	_DS18B20_H_
 
-unsigned char Reset(void);
-unsigned char ReadBit(void);
-void WriteBit(unsigned char dBit);
-unsigned char ReadByte(void);
-void WriteByte(unsigned char Dout);
-int ReadTemp(void);
+// --- Public 1-Wire Transmission Methods API ---
+unsigned char Reset(void);                 // Dispatches bus reset sequence pulses
+unsigned char ReadBit(void);                // Extracts individual status bit components out from data frames
+void WriteBit(unsigned char dBit);         // Drives targeted bit logic outputs across the line network
+unsigned char ReadByte(void);               // Aggregates bits to assemble standard data bytes
+void WriteByte(unsigned char Dout);        // Serializes full bytes out onto active signal pins
+int ReadTemp(void);                        // Master control layer extracting raw hex data conversions
 
 #endif
